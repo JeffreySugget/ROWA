34f847133398950ab55b5e2ab061c18f56a5948c
unknown <jeffrey.sugget@gmail.com>
2019-02-23 16:51:49 +0800

    Created Trevor memorial page

---
+++ rowa/Controllers/MemorialRideController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rowa.Controllers
+{
+    public class MemorialRideController : Controller
+    {
+        // GET: MemorialRide
+        public ActionResult MemorialRideInfo()
+        {
+            return View();
+        }
+    }
+}
