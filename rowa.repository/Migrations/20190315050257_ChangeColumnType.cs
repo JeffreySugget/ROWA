f79f834bb8864304a2af265d79a3c59cda544a4a
Jeff Sugget <Jeff.Sugget@asggroup.com.au>
2019-03-15 13:03:42 +0800

    Created migration for column type change

---
+++ rowa.repository/Migrations/20190315050257_ChangeColumnType.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace rowa.repository.Migrations
+{
+    public partial class ChangeColumnType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "ExecutionTime",
+                table: "PerformanceLog",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ExecutionTime",
+                table: "PerformanceLog",
+                nullable: true,
+                oldClrType: typeof(double));
+        }
+    }
+}
