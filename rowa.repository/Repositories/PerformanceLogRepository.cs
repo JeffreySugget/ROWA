aa72a2fbd4c23c94d3a3378e18426deaca417bc9
Jeff Sugget <Jeff.Sugget@asggroup.com.au>
2019-03-15 12:44:23 +0800

    Created performance log repository

---
+++ rowa.repository/Repositories/PerformanceLogRepository.cs
@@ -0,0 +1,15 @@
+﻿using rowa.repository.Entities;
+using rowa.repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rowa.repository.Repositories
+{
+    public class PerformanceLogRepository : BaseRepository<PerformanceLog>, IPerformanceLogRepository
+    {
+
+    }
+}
