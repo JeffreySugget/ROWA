1a2a3cd115b10813263939ebab42b7cfa6b5c152
jeffrey.sugget <jeffrey.sugget@gmail.com>
2019-04-20 11:12:04 +0800

    Created sign in view, created user helper class

---
+++ rowa.repository/Classes/IUserHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rowa.repository.Classes
+{
+    public interface IUserHelper
+    {
+        string EncryptPassword(string password);
+    }
+}
---
+++ rowa/Controllers/SignInController.cs
@@ -0,0 +1,30 @@
+﻿using rowa.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rowa.Controllers
+{
+    public class SignInController : Controller
+    {
+        // GET: SignIn
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogIn(SignInModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index", model);
+            }
+
+            return View("Index");
+        }
+    }
+}
