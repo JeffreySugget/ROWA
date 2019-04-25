ece0f936b25b846dbcf851df675fd771ec73bd65
Jeffrey Sugget <jeffrey.sugget@gmail.com>
2019-04-25 14:54:03 +0800

    Created controller and view for CreateRoute

---
+++ rowa/Controllers/RideRouteController.cs
@@ -0,0 +1,36 @@
+﻿using rowa.Filters;
+using rowa.Models;
+using rowa.repository.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace rowa.Controllers
+{
+    [UserInRole(RoleConsts.Admin)]
+    public class RideRouteController : Controller
+    {
+        public ActionResult CreateRoute()
+        {
+            return View(new CreateRouteModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CreateRouteModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CreateRoute", model);
+            }
+
+            var temp = model.WayPoints;
+
+            ViewBag.SuccessMessage = "Successfully Created Route";
+
+            return View("CreateRoute", model);
+        }
+    }
+}
---
+++ rowa/Models/CreateRouteModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace rowa.Models
+{
+    public class CreateRouteModel
+    {
+        [DisplayName("Start Point"), Required]
+        public string StartPoint { get; set; }
+
+        [DisplayName("End Point"), Required]
+        public string EndPoint { get; set; }
+
+        [DisplayName("Way Points"), Required]
+        public string WayPoints { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
